9a5958d0df40f9dcfc00cef6e607b7c7bd3eeeeb
hardyyb2 <hardikb3732@gmail.com>
2021-02-12 12:37:50 +0530

    [ADDED] Font and GlobalStyles

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 
 import { Routes } from "../Routes";
 
+import "../styles/GlobalStyles.css";
+import "../styles/TypoGraphy.css";
+
 import styles from "./App.module.css";
 
 const App = ({ isVerifying, isAuthenticated, verifyUserConnect }) => {
