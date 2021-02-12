7c9e58261b97b9dabdf23b045cd591829fe1c82a
hardyyb2 <hardikb3732@gmail.com>
2021-02-12 13:47:53 +0530

    [ADDED] Routes Object

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,18 +7,39 @@ import { Spinner } from "../common";
 const Login = lazy(() => import("../pages/login"));
 const Home = lazy(() => import("../pages/home"));
 
+const routes = [
+  {
+    component: Home,
+    path: "/",
+    exact: true,
+    isProtected: true,
+  },
+  {
+    component: Login,
+    path: "/login",
+    exact: false,
+    isProtected: false,
+  },
+];
+
 const Routes = ({ isAuthenticated, isVerifying }) => {
   return (
     <Switch>
       <Suspense fallback={<Spinner />}>
-        <ProtectedRoute
-          exact
-          path="/"
-          component={Home}
-          isAuthenticated={isAuthenticated}
-          isVerifying={isVerifying}
-        />
-        <Route path="/login" component={Login} />
+        {routes &&
+          routes.map(({ component, path, exact, isProtected }, index) =>
+            isProtected ? (
+              <ProtectedRoute
+                exact={exact}
+                path={path}
+                component={component}
+                isAuthenticated={isAuthenticated}
+                isVerifying={isVerifying}
+              />
+            ) : (
+              <Route path={path} component={component} />
+            )
+          )}
       </Suspense>
     </Switch>
   );
